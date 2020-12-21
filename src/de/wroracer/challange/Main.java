e77696f2b7eef88cea1c59b97f1e0c58865fbdaa
WROracer <67609673+WROracer@users.noreply.github.com>
2020-12-21 10:37:17 +0100

    Init funktional Plugin

---
+++ src/de/wroracer/challange/Main.java
@@ -0,0 +1,20 @@
+package de.wroracer.challange;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private static Main plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
